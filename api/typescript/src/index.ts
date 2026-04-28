9464e90a95b3b975ba9a0b4df5c825a3c1dce47a
You Shimajiro <you1025@gmail.com>
2026-04-28 18:05:39 +0900

    api ディレクトリを作成

--- api/typescript/src/index.ts
+++ api/typescript/src/index.ts
@@ -5,5 +5,5 @@ import app from "./app.js";
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://loocalhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}/api/`);
 });
